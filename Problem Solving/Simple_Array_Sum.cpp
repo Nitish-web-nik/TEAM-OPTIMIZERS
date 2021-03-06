092775ec05407b46c96a94f3e59b57e249507738
Nitish Kumar <57797801+Nitish-web-nik@users.noreply.github.com>
2021-03-06 18:59:28 +0530

    Add files via upload

---
+++ Problem Solving/Simple_Array_Sum.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int size, i, sum = 0;
+    cin>>size;
+    int arr[size];
+    for(i = 0; i < size ; i++)
+    {
+        cin>>arr[i];
+    }
+    for(i = 0; i < size; i++)
+    {
+        sum += arr[i];
+    }
+    cout<<sum;
+    return 0;
+}
+
+/*Output:-
+5
+1
+2
+3
+4
+5
+15
+*/

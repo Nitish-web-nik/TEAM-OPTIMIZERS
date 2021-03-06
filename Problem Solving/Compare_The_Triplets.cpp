1e3b2173b700329b4ccd8d74a87af89e1e28d391
Nitish Kumar <57797801+Nitish-web-nik@users.noreply.github.com>
2021-03-06 19:28:24 +0530

    Add files via upload

---
+++ Problem Solving/Compare_The_Triplets.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int A[3], B[3],i, count_a = 0,count_b = 0;
+    for(i = 0; i < 3; i++)
+    {
+        cin>>A[i];
+    }
+    for(i = 0; i < 3; i++)
+    {
+        cin>>B[i];
+    }
+    for(i = 0; i < 3; i++)
+    {
+        if(A[i] > B[i])
+        {
+            count_a++;
+        }
+        else if(A[i] < B [i])
+        {
+            count_b++;
+        }
+        else
+        {
+        }
+    }
+    cout<<count_a;
+    cout<<count_b;
+    return 0;
+}

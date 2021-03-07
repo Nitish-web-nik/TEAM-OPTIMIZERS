50efdc972cc5f2c7ec60bc6baf2c22f27ec2529a
Nitish Kumar <57797801+Nitish-web-nik@users.noreply.github.com>
2021-03-07 19:48:23 +0530

    Add files via upload

---
+++ Problem Solving/Diagonal_Difference.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int n, i,j, sum1 = 0, sum2 = 0;
+    cin>>n;
+    int arr[n][n];
+    for( i = 0 ; i < n ; i++)
+    {
+        for( j = 0 ; j < n ; j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+    for( i = 0 ; i < n ; i++)
+    {
+        for(j = 0; j < n; j++)
+        {
+            if(i == j)
+            {
+                sum1 = sum1 + arr[i][j];
+            }
+            if(i == n - j - 1)
+            {
+                sum2 = sum2 + arr[j][i];
+            }
+        }
+    }
+
+    int max = abs(sum1 - sum2);
+    cout<<max;
+
+}

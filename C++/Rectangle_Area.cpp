153043a521319670a46944cc50aff2eb00d76b30
Nitish Kumar <57797801+Nitish-web-nik@users.noreply.github.com>
2021-03-08 21:30:59 +0530

    Add files via upload

---
+++ C++/Rectangle_Area.cpp
@@ -0,0 +1,65 @@
+/*Sample Input
+
+10 5
+Sample Output
+
+10 5
+50
+*/
+#include <iostream>
+
+using namespace std;
+/*
+ * Create classes Rectangle and RectangleArea
+ */
+class Rectangle
+{
+protected:
+    int width, height;
+public:
+    void display()
+    {
+        cout<<width<<" ";
+        cout<<height<<endl;
+    }
+};
+class RectangleArea : public Rectangle
+{
+public:
+    void read_input()
+    {
+        cin>>width;
+        cin>>height;
+    }
+    void display()
+    {
+        int area;
+        area = width * height;
+        cout<<area;
+    }
+};
+
+int main()
+{
+    /*
+     * Declare a RectangleArea object
+     */
+    RectangleArea r_area;
+
+    /*
+     * Read the width and height
+     */
+    r_area.read_input();
+
+    /*
+     * Print the width and height
+     */
+    r_area.Rectangle::display();
+
+    /*
+     * Print the area
+     */
+    r_area.display();
+
+    return 0;
+}

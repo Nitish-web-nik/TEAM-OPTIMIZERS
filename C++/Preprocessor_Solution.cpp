2b6841b6cbcb976e72d474ca92c4c8471ca467a9
Nitish Kumar <57797801+Nitish-web-nik@users.noreply.github.com>
2021-03-08 20:29:51 +0530

    Add files via upload

---
+++ C++/Preprocessor_Solution.cpp
@@ -0,0 +1,43 @@
+#define INF 1000000000
+
+#define FUNCTION(f, comp) \
+        void f(int& a, int b) \
+        { if (b comp a) a = b; }
+
+#define io(v) \
+    cin >> v
+
+#define toStr(x) #x
+
+#define foreach(v, i) \
+    for (size_t i = 0; i < v.size(); ++i)
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#if !defined toStr || !defined io || !defined FUNCTION || !defined INF
+#error Missing preprocessor definitions
+#endif
+
+FUNCTION(minimum, <)
+FUNCTION(maximum, >)
+
+int main(){
+	int n; cin >> n;
+	vector<int> v(n);
+	foreach(v, i) {
+		io(v)[i];
+	}
+	int mn = INF;
+	int mx = -INF;
+	foreach(v, i) {
+		minimum(mn, v[i]);
+		maximum(mx, v[i]);
+	}
+	int ans = mx - mn;
+	cout << toStr(Result =) <<' '<< ans;
+	return 0;
+
+}

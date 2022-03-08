fa7b6427fd239261cec85a94a7ff9531ab06cad2
Nitish Kumar <krnitish1207@gmail.com>
2022-03-08 11:59:34 +0530

    binary_search

--- Problem Solving/Binary_search.cpp
+++ Problem Solving/Binary_search.cpp
@@ -2,22 +2,8 @@
 using namespace std;
 int main()
 {
-    int arr[5]={4,8,11,13,17}, mid = 2,key,flag;
-    cin>>key;
-    for(int i = 0;i<5;i++)
-    {
-        if(arr[mid]==key)
-        {
-            cout<<"The key is present at index "<<mid;
-            flag = 0;
-            break;
-        }
-        else
-            mid /= 2;
-    }
-    if(flag != 0)
-    {
-        cout<<"Element is not present ";
-    }
+    int arr[12],n,flag,key;
+    cout<<"Enter the size of the array ";
+    cin>>
     return 0;
 }
